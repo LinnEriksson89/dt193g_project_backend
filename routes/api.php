88c4bfde986af0289d10602c44da782a22a74db7
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2026-02-13 15:33:55 +0100

    adding routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use App\Http\Controllers\MovieController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CategoryConnectionsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +9,18 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('movie', MovieController::class)->missing(function (Request $request) {
     return response()->json([
         "Filmen hittades inte."],
+        404);
+});
+
+Route::apiResource('category', CategoryController::class)->missing(function (Request $request) {
+    return response()->json([
+        "Kategorin hittades inte."],
+        404);
+});
+
+Route::apiResource('connection', CategoryConnectionsController::class)->missing(function (Request $request) {
+    return response()->json([
+        "Ingen koppling hittades."],
         404);
 });
 
