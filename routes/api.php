4ed4767825888477b3636867969f4e3b178e83cd
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2026-02-13 15:28:13 +0100

    adding controllers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,14 @@
 <?php
-
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::apiResource('movie', MovieController::class)->missing(function (Request $request) {
+    return response()->json([
+        "Filmen hittades inte."],
+        404);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
