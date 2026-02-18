5a9c48f950fe9bc2ba0a3e1b09c7cecca3732053
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2026-02-18 12:09:49 +0100

    Renamed CategoryConnections and added functionality

---
+++ app/Http/Controllers/CategoryConnectionController.php
@@ -0,0 +1,68 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryConnectionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CategoryConnection::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            "categoryid" => "required",
+            "movieid"    => "required",
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CategoryConnection $connection)
+    {
+        $id              = $connection->id;
+        $foundConnection = CategoryConnection::find($id);
+
+        return $foundConnection;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CategoryConnection $connection)
+    {
+        $request->validate([
+            "categoryid" => "required",
+            "movieid"    => "required",
+        ]);
+
+        $id              = $connection->id;
+        $foundConnection = CategoryConnection::find($id);
+        $foundConnection->update($request->all());
+
+        return $foundConnection;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CategoryConnection $connection)
+    {
+        $id              = $connection->id;
+        $foundConnection = CategoryConnection::find($id);
+        $foundConnection->delete();
+
+        return response()->json([
+            "Filmen har tagits bort ur kategorin.",
+        ]);
+    }
+}
--- app/Models/CategoryConnection.php
+++ app/Models/CategoryConnection.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CategoryConnections extends Model
+class CategoryConnection extends Model
 {    
     protected $fillable = [
         "categoryid",
